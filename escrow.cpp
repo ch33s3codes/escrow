8880af2c0d05b4daaa9aff1754be1a96decdf86c
I am The Cheese <ch33s3codes@gmail.com>
2018-11-12 21:32:14 +0300

    Restructure - header and implementation

---
+++ escrow.cpp
@@ -0,0 +1,85 @@
+#include "escrow.hpp"
+
+ACTION escrow::initiatedeal(name sponsor, name contractor,
+                          name arbiter, string memo, asset amount,
+                          uint64_t no_of_sponsors, uint64_t threshold,
+                          time_point_sec timeout_of_funding, time_point_sec timeout_of_acceptance,
+                          vector<uint64_t> payout_schedule, time_point_sec payout_term)
+{
+    eosio_assert(is_account(contractor), "enter a valid EOS account");
+    eosio_assert(is_account(arbiter), "arbitrators must be a valid EOS account holders");
+
+    require_auth(sponsor);
+
+    _deals.emplace(get_self(), [&](auto &d) {
+        d.id = _deals.available_primary_key();
+        d.sponsor = sponsor;
+        d.contractor = contractor;
+        d.arbiter = arbiter;
+        d.memo = memo;
+        d.amount = amount;
+        d.no_of_sponsors = no_of_sponsors;
+        d.threshold = threshold;
+        d.timeout_of_funding = timeout_of_funding;
+        d.timeout_of_acceptance = timeout_of_acceptance;
+        d.payout_schedule = payout_schedule;
+        d.payout_term = payout_term;
+    });
+}
+
+ACTION escrow::addsponsors(name sponsor, uint64_t &dealId, std::vector<name> coSponsors)
+{
+    require_auth(sponsor);
+    eosio_assert(coSponsors.size() > 0, "this action requires at least one cosponsor");
+
+    auto i = _deals.find(dealId);
+    if (i != _deals.end())
+    {
+        _deals.modify(i, sponsor, [&](auto &d) {
+            d.coSponsors = coSponsors;
+        });
+    }
+}
+
+ACTION escrow::checkdeposit(name from, name to, asset quantity, string memo)
+{
+    if (from == _self)
+    {
+        // Handle refunds?
+        return;
+    }
+
+    eosio_assert(to == _self, "contract is not involved in this transfer"); // TODO - Handle refunds as well
+    eosio_assert(quantity.symbol.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "only positive quantity allowed");
+    eosio_assert(quantity.symbol == EOS_SYMBOL, "only EOS tokens allowed"); // TODO - Handle other tokens as well. Dont Lock to EOS alone.
+
+    eosio_assert(isNumeric(memo), "the memo needs to represent the claim ID");
+
+    uint64_t dealId = std::stoi(memo);
+    if (from != _self)
+    {
+        _deposits.emplace(get_self(), [&](auto &dep) {
+            dep.id = _deposits.available_primary_key();
+            dep.dealId = dealId;
+            dep.sponsor = from;
+            dep.amount = quantity;
+        });
+    }
+}
+
+extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action)
+{
+    if (code == "eosio.token"_n.value && action == "transfer"_n.value)
+    {
+        eosio::execute_action(
+            eosio::name(receiver), eosio::name(code), &escrow::checkdeposit);
+    }
+    else if (code == receiver)
+    {
+        switch (action)
+        {
+            EOSIO_DISPATCH_HELPER(escrow, (initiatedeal)(addsponsors))
+        }
+    }
+}
---
+++ escrow.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/time.hpp>
+#include <eosiolib/action.hpp>
+#include <eosiolib/multi_index.hpp>
+
+#define EOS_SYMBOL symbol("EOS", 4)
+
+using namespace std;
+using namespace eosio;
+using eosio::action;
+
+CONTRACT escrow : public eosio::contract {
+    public:
+        using contract::contract;
+
+        escrow(eosio::name self, eosio::name code, eosio::datastream<const char *> ds): eosio::contract(self, code, ds), _deals(self, code.value), _deposits(self, code.value){}
+        
+        const float FEE = 0.1; // 1% charged as fees for arbiter and escrow account.
+
+
+        bool isNumeric(const std::string& input) {
+            return std::all_of(input.begin(), input.end(), ::isdigit);
+        }
+
+        ACTION initiatedeal(name sponsor, name contractor, 
+                          name arbiter, string memo, asset amount, 
+                          uint64_t no_of_sponsors, uint64_t threshold, 
+                          time_point_sec timeout_of_funding, time_point_sec timeout_of_acceptance, 
+                          vector<uint64_t> payout_schedule, time_point_sec payout_term);
+
+        ACTION addsponsors(name sponsor, uint64_t &dealId, std::vector<name> coSponsors);
+
+
+        void checkdeposit(
+            eosio::name from,
+            eosio::name to,
+            eosio::asset quantity,
+            std::string memo
+        );
+
+        void get_payable(uint64_t dealId){
+            
+        }
+    
+
+    private:
+       TABLE deal
+        {
+            uint64_t id;
+            name sponsor;
+            name contractor;
+            name arbiter;
+            std::vector<name> coSponsors;
+            string memo;
+            asset amount;
+            uint64_t no_of_sponsors;
+            uint64_t threshold;
+            time_point_sec timeout_of_funding;
+            time_point_sec timeout_of_acceptance;
+            vector<uint64_t> payout_schedule;
+            time_point_sec payout_term;
+
+            uint64_t primary_key() const { return id; }
+            uint64_t by_sponsor() const { return sponsor.value;}
+            uint64_t by_contractor() const {return contractor.value; }
+        };
+    typedef eosio::multi_index<"deals"_n, deal, 
+            eosio::indexed_by<"sponsor"_n, eosio::const_mem_fun<deal, uint64_t, &deal::by_sponsor>>,
+            eosio::indexed_by<"contractor"_n, eosio::const_mem_fun<deal, uint64_t, &deal::by_contractor>>> deals_table;
+
+    TABLE deposit {
+        uint64_t id;
+        uint64_t dealId;
+        name sponsor;
+        asset amount;
+
+        uint64_t primary_key() const { return id;}
+        uint64_t by_dealId() const {return dealId; }
+        uint64_t by_sponsor() const { return sponsor.value; }
+    };
+    
+    typedef eosio::multi_index<"deposits"_n, deposit,
+            eosio::indexed_by<"dealid"_n, eosio::const_mem_fun<deposit, uint64_t, &deposit::by_dealId>>,
+            eosio::indexed_by<"sponsor"_n, eosio::const_mem_fun<deposit, uint64_t, &deposit::by_sponsor>>> deposits_table;
+
+
+    deals_table _deals;
+    deposits_table _deposits;
+};
